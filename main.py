92003202d4b4bdaa0d46ad5b72eb7fd921a635d6
Selitha-Kalka <bunnysmasher@gmail.com>
2025-01-23 14:36:48 -0800

    finished chr cout func & defined chr sorter and reporter

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(word_count(file_contents))
+        print(character_count(file_contents))
 
-def word_count(string):
-    words = string.split()
+def word_count(s):
+    words = s.split()
     return len(words)
+
+def character_count(s):
+    count = {}
+    j = s.lower()
+    for i in j:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
+    return count
+
+def characher_sorter(d):
+    pass
+
+def cat_report(w, c):
+    pass
+
 
 main()
